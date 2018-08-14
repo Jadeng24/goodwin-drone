5ece8bdb41d403bf29d740667ae0615d5ed3440f
Jaden Goodwin <jaden.goodwin24@gmail.com>
2018-08-14 13:13:41 -0600

    creating home page layout with video triangle overlay

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Footer.css';
+ 
+class Footer extends Component{ 
+    constructor(){
+        super();
+ 
+        this.state = {
+            
+        }
+    }
+ 
+    render(){
+        return (
+            <div className='Footer'>
+                <div className="FooterBtns"></div>
+            </div>
+        )
+    }
+}
+export default Footer;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Home.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Nav from '../Nav/Nav.js';
+import Footer from '../Footer/Footer.js';
 class Home extends Component {
     constructor() {
         super();
@@ -15,16 +16,21 @@ class Home extends Component {
         return (
             <div className='Home'>
                 <div className="HomeContent">
-                    <Nav />
-
-                    <div className="greyTriangle">
+                    <div className="fixedNav"><Nav /></div>
+                    <div className='videoHolder'>
+                        <div className="whiteTriangle">
+                        </div>
+                    </div>
+                    <div className="whiteDiv">
                     </div>
 
 
 
 
 
+                
                 </div>
+                <Footer/>
             </div>
         )
     }
